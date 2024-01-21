b1b07783d9d5ec41dadf62c1a8dcd2f6985850a7
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-01-21 21:24:28 +0800

    Added Formik and Bootstrap

--- spiceyourslice/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div style={{padding: '10px 50px', background:'black'}}>
-        <div style={{color:"white"}}>Pizza Customization</div>
-    </div>
-  )
-}
