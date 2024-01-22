5800d68b2dccf37684b4ca160c5f8851d3207e9e
Mary Balais <95016909+TheMoreTheMary-er@users.noreply.github.com>
2024-01-22 21:08:00 +0800

    Finished added JSON Functions

--- spiceyourslice/src/components/Summary.js
+++ spiceyourslice/src/components/Summary.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Card, Form, Row, Col, Button} from 'react-bootstrap';
-export default function Summary() {
+export default function Summary(props) {
+  const {formik} = props;
   return (
     <Card>
       <Card.Img variant="top" src="https://images.unsplash.com/photo-1593504049359-74330189a345?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <Card.Body>
-        <Card.Title>JSON</Card.Title>
+        <Card.Title>Order Summary</Card.Title>
         <Card.Text>
-          <pre>JSON</pre>
+          <pre >{JSON.stringify(formik.values, null, 2)}</pre>
         </Card.Text>
       
       </Card.Body>
