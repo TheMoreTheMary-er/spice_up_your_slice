38bac3b3cfdd1badb48a27f841dcf713bfd88372
Mary Balais <95016909+TheMoreTheMary-er@users.noreply.github.com>
2024-01-22 21:33:46 +0800

    Completed Pizza Designer

--- spiceyourslice/src/App.js
+++ spiceyourslice/src/App.js
@@ -1,60 +1,66 @@
-import {Nav, Card, Container, Form, Row,Col} from 'react-bootstrap';
+import { useFormik } from "formik";
+import { Card, Col, Container, Form, Nav, Row } from "react-bootstrap";
 import Order from "./components/Order";
 import Summary from "./components/Summary";
-import {useFormik} from "formik";
 
-const validate=() => {}; 
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.size) {
+    errors.size = "Required size";
+  }
+  if (!values.crust) {
+    errors.crust = "Required crust";
+  }
+  return errors;
+};
 
 function App() {
-
   const formik = useFormik({
     initialValues: {
-      size: " ",
-      crust:" ",
-      cheese:{
-        includes: false, value: "",
-      },
-      sauce:{
+      size: "",
+      crust: "",
+      cheese: {
         includes: false,
-        value:"",
+        value: "normal",
+      },
+      sauce: {
+        includes: false,
+        value: "robustInspiredTomatoSauce",
       },
       toppingMeat: [],
       toppingNonMeat: [],
-      specialInstruction: " ",
+      specialInstruction: "",
     },
-    onSubmit: (values) => { },
-    validate
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validate,
   });
 
   return (
     <div>
-     <Nav
-      activeKey="/home"
-      style={{background:"#0d6efd"}}>
-      <Nav.Item>
-        <Nav.Link href="/home" style={{color:"white"}}>
-          <h4> Pizza Builder Form</h4>
-        
-        </Nav.Link>
-      
-      </Nav.Item>
-    </Nav>
-    <Container className="my-4">
-    <Card className="shadow-lg p-4">
-      <Form onSubmit={formik.handleSubmit}>
-        <Row>
-          <Col md={8}> 
-          <Order formik={formik}></Order>
-          </Col>
-          <Col md={4}>
-          <Summary formik={formik}></Summary>
-          </Col>
-        </Row>
-      </Form>
-      
-    </Card>
-    </Container>
-
+      <Nav activeKey="/home" style={{ background: "#0d6efd" }}>
+        <Nav.Item>
+          <Nav.Link href="/home" style={{ color: "#ffff" }}>
+            <h4>PIZZA BUILDER FORM</h4>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container className="my-4">
+        <Card className="shadow-lg p-4">
+          <Form onSubmit={formik.handleSubmit}>
+            <Row>
+              <Col md={8}>
+                <Order formik={formik}></Order>
+              </Col>
+              <Col md={4}>
+                <Summary formik={formik}></Summary>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </Container>
     </div>
   );
 }
--- spiceyourslice/src/components/Summary.js
+++ spiceyourslice/src/components/Summary.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Card, Form, Row, Col, Button} from 'react-bootstrap';
-export default function Summary(props) {
+
+
+
+ function Summary(props) {
   const {formik} = props;
   return (
     <Card>
-      <Card.Img variant="top" src="https://images.unsplash.com/photo-1593504049359-74330189a345?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card.Img variant="top" 
+      src="https://images.unsplash.com/photo-1593504049359-74330189a345?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <Card.Body>
         <Card.Title>Order Summary</Card.Title>
         <Card.Text>
@@ -12,7 +16,11 @@ export default function Summary(props) {
         </Card.Text>
       
       </Card.Body>
-      <Button variant="danger" type="Submit">Place Order</Button>
+      <Button variant="danger" type="Submit">
+        Place Order
+      </Button>
     </Card>
   )
 }
+
+export default Summary;
